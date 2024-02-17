4137db0944b769932ae0f4c442c7c271405dfa07
Jason Lin <jclin2.2009@gmail.com>
2024-02-17 14:54:32 -0800

    yuuup

--- book-scan-frontend/app/components/docStream.tsx
+++ book-scan-frontend/app/components/docStream.tsx
@@ -4,7 +4,6 @@ import { Box, Image, VStack, Text, Button } from "@chakra-ui/react";
 import Lottie from "lottie-react";
 import bookFlipAnimation from "../../public/book-flip.json";
 import Link from "next/link";
-import { get } from "http";
 
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
@@ -14,6 +13,8 @@ const supabase = createClient(
 export default function DocStream() {
   const [imageUrl, setImageUrl] = useState<string>("");
   const [docId, setDocId] = useState<string>("");
+  const [scanningStarted, setScanningStarted] = useState(false); // State to track scanning status
+
 
   useEffect(() => {
     // Function to fetch and set the image URL
@@ -40,6 +41,7 @@ export default function DocStream() {
         { event: "INSERT", schema: "public", table: "images" },
         async (payload) => {
           console.log("Change received!", payload);
+          setScanningStarted(true); // Update scanning state when a new image is inserted
           await fetchAndSetImageUrl(payload.new.image_name);
           await setDocId(payload.new.doc_id);
         }
@@ -50,15 +52,15 @@ export default function DocStream() {
     return () => {
       supabase.removeChannel(img_subscription);
     };
-  }, []); // Empty dependency array to run only once on mount
+  }, [scanningStarted]); // Empty dependency array to run only once on mount
 
   console.log(docId);
   return (
     <VStack maxH="50%">
       <Box>
-        {/* {imageUrl ? ( */}
-          <>
-            <Box pos="relative">
+        {imageUrl ? (
+          <VStack>
+            <Box pos="relative" borderRadius="10px">
               <Image src={imageUrl} alt="Document" />
             </Box>
             <Button
@@ -69,13 +71,13 @@ export default function DocStream() {
             >
               Analyze PDF
             </Button>
-          </>
-        {/* ) : (
+          </VStack>
+        ) : (
           <VStack id="lottie" maxW="xl" maxH="xl">
             <Lottie animationData={bookFlipAnimation} loop={true} />
             <Text fontStyle="italic">Waiting for your scan...</Text>
           </VStack>
-        )} */}
+        )}
       </Box>
     </VStack>
   );
