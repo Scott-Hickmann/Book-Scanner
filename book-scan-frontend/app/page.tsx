c7ba5e376434ab9e5c7a847c30bcfac55a46ec88
Jason Lin <jclin2.2009@gmail.com>
2024-02-17 14:08:28 -0800

    preview pdf

--- book-scan-frontend/app/page.tsx
+++ book-scan-frontend/app/page.tsx
@@ -1,19 +1,24 @@
-// app/page.tsx
 "use client";
 
-import { Heading, VStack, Text, Box, Button, Icon } from "@chakra-ui/react";
-
-import { ArrowForwardIcon } from "@chakra-ui/icons";
+import {
+  Heading,
+  VStack,
+  Text,
+  Box,
+  Button,
+  Icon,
+} from "@chakra-ui/react";
 import DocStream from "./components/docStream";
 
 export default function Page() {
+
   return (
-    <VStack h="100%" >
+    <VStack h="100%">
       <VStack p={5} borderBottom="1px" borderColor="gray.200" h="100%">
         <Heading size="3xl">Recollect</Heading>
         <Text fontSize="xl">Your Memories, Preserved Forever</Text>
       </VStack>
-      <DocStream/>
+      <DocStream />
     </VStack>
   );
 }
