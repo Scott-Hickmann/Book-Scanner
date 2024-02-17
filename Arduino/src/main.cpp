db08531268e3171a5cb6ad6575f3725a3a1cb8c8
Scott Hickmann <scotthickmann21@gmail.com>
2024-02-16 23:12:31 -0800

    Serial servo communication

--- Arduino/src/main.cpp
+++ Arduino/src/main.cpp
@@ -1,39 +1,19 @@
 #include <Servo.h>
-
+#include <Arduino.h>
 
 Servo myservo;  // create servo object to control a servo
 
-// twelve servo objects can be created on most boards
-
-
-int pos = 0;    // variable to store the servo position
-
-
 void setup() {
-
   myservo.attach(9);  // attaches the servo on pin 9 to the servo object
-
+  Serial.begin(9600); // initialize serial communication at 9600 bits per second:
 }
 
-
 void loop() {
-
-  for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
-
-    // in steps of 1 degree
-
-  myservo.write(pos);              // tell servo to go to position in variable 'pos'
-
-    delay(15);                       // waits 15ms for the servo to reach the position
-
+  if (Serial.available() > 0) {        // check if data is available to read
+    int position = Serial.parseInt();  // read the incoming integer
+    if (position >= 0 && position <= 180) { // check if the position is within the servo range
+      myservo.write(position);         // tell servo to go to position
+    }
+    Serial.println(position);          // print the position
   }
-
-  for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
-
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-
-    delay(15);                       // waits 15ms for the servo to reach the position
-
-  }
-
 }
