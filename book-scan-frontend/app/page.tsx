a93e5159b31abc2fc973264da700bb41e8f2eb0f
Jason Lin <jclin2.2009@gmail.com>
2024-02-17 00:44:24 -0800

    styling

--- book-scan-frontend/app/page.tsx
+++ book-scan-frontend/app/page.tsx
@@ -1,9 +1,10 @@
 // app/page.tsx
 "use client";
 
-import { Heading, VStack, Text, Box } from "@chakra-ui/react";
+import { Heading, VStack, Text, Box, Button, Icon } from "@chakra-ui/react";
 import Lottie from "lottie-react";
 import bookFlipAnimation from "../public/book-flip.json";
+import { ArrowForwardIcon } from "@chakra-ui/icons";
 
 export default function Page() {
   return (
@@ -12,9 +13,12 @@ export default function Page() {
         <Heading size="3xl">Recollect</Heading>
         <Text fontSize="xl">Your Memories, Preserved Forever</Text>
       </VStack>
-      <Box id="pdf-area" maxW="lg" maxH="lg">
+      <Box id="lottie" maxW="xl" maxH="xl">
         <Lottie animationData={bookFlipAnimation} loop={true} />
       </Box>
+      <Button>
+        Start Scanning <Icon as={ArrowForwardIcon} ml={2} />
+      </Button>
     </VStack>
   );
 }
