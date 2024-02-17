1161cc7fa1ab6050902285c3980cf29803097dcd
Scott Hickmann <scotthickmann21@gmail.com>
2024-02-17 15:51:10 -0800

    Better avoid glass

--- Controller/__main__.py
+++ Controller/__main__.py
@@ -4,9 +4,6 @@ from joined_servo import JoinedServo
 import time
 import itertools
 
-INITIAL_TURNER_MAIN_POSITION = 90
-INITIAL_TURNER_SECONDARY_POSITION = 180
-
 
 def calibrate_glass(servo: Servo, initial_position):
     while True:
@@ -49,10 +46,8 @@ def calibrate_turner(servoTurnerMain: Servo, servoTurnerSecondary: Servo):
 if __name__ == "__main__":
     try:
         servo_manager = ServoManager()
-        servoTurnerMain = Servo(0, servo_manager, INITIAL_TURNER_MAIN_POSITION)
-        servoTurnerSecondary = Servo(
-            1, servo_manager, INITIAL_TURNER_SECONDARY_POSITION
-        )
+        servoTurnerMain = Servo(0, servo_manager, 20)
+        servoTurnerSecondary = Servo(1, servo_manager, 180)
         servoGlassLeft = Servo(2, servo_manager, 0)
         servoGlassRight = InvertedServo(3, servo_manager, 0)
         servoGlass = JoinedServo([servoGlassLeft, servoGlassRight])
@@ -67,9 +62,9 @@ if __name__ == "__main__":
             0, speed=5
         )  # Bring the glass back up to avoid hitting the turner
         servoTurnerMain.glide_position(
-            INITIAL_TURNER_MAIN_POSITION, speed=5
+            40, speed=5
         )  # Bring the turner back to its initial position
-        servoTurnerSecondary.glide_position(INITIAL_TURNER_SECONDARY_POSITION, speed=5)
+        servoTurnerSecondary.glide_position(180, speed=5)
 
         print("Calibrating turner")
         calibrate_turner(servoTurnerMain, servoTurnerSecondary)
@@ -83,9 +78,12 @@ if __name__ == "__main__":
             # 1. Turn the page
             servoTurnerMain.glide_position(100, speed=3)
             # 2. Release the page
-            servoTurnerSecondary.glide_position(120, speed=5)
+            servoTurnerSecondary.glide_position(90, speed=5)
+            time.sleep(0.5)  # Wait for the page to fall
+            servoTurnerSecondary.glide_position(30, speed=5)
             # 3. Move out of the way for the glass
             servoTurnerMain.glide_position(servoTurnerMain.initial_position, speed=5)
+            servoTurnerSecondary.glide_position(100, speed=5)
             # 4. Bring the glass down to flatten the page
             servoGlass.glide_position(servoGlass.initial_position, speed=10)
             # 5. Scan the page
